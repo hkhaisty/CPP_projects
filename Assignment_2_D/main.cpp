f4c756fb65062421b8845286aa1129d11d80bc63
hkhaisty <h.k.haisty@gmail.com>
2018-09-30 23:05:52 -0400

    adds rest of files

--- Assignment_2_D/main.cpp
+++ Assignment_2_D/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+//this code works as intended, so I made no modifications
+int* reverse(const int* list, int size)
+{
+    int* result = new int[size];
+    for (int i = 0, j = size - 1; i < size; i++, j--)
+    {
+        result[j] = list[i];
+    }
+    return result;
+}
+void printArray(const int* list, int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << list[i] << " ";
+}
+int main()
+{
+    int list[] = {1, 2, 3, 4, 5, 6};
+    printArray(list, 6);
+    cout << endl;
+    int* p = reverse(list, 6);
+    printArray(p, 6);
+    cout << endl;
     return 0;
 }
