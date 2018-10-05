1b1b15a3710750cde8aa8cc639decfcf3a07e623
hkhaisty <h.k.haisty@gmail.com>
2018-10-04 22:21:13 -0400

    adds solution to lab4

--- Lab_5/main.cpp
+++ Lab_5/main.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
+#include <chrono>
+#include <numeric>
+#include <vector>
 
 using namespace std;
-
 static const int N = 1000;
 
 int main() {
-    int i, a[N];
-    for (i = 2; i < N; i++) a[i] = 1;
-    for (i = 2; i < N; i++)
-        if (a[i])
-            for (int j = i; j * i < N; j++) a[i * j] = 0;
+    for (auto size = 1ull; size < 1000000000ull; size *= 100) {
+        // record start time
+        auto start = std::chrono::system_clock::now();
+        int i, a[N];
+        for (int i = 2; i < N; i++) a[i] = 1;
+        for (i = 2; i < N; i++)
+            if (a[i])
+                for (int j = i; j * i < N; j++) a[i * j] = 0;
 
-    for (i = 2; i < N; i++)
+        auto start_2 = std::chrono::system_clock::now();
 
-        if (a[i]) cout << " " << i;
-    cout << endl;
+        // record end times
+        auto end = std::chrono::system_clock::now();
+        auto end_2 = std::chrono::system_clock::now();
+
+        //calculate differences in time
+        std::chrono::duration<double> diff = end - start;
+        std::chrono::duration<double> diff_2 = end_2 - start_2;
+
+        //print out differences
+        std::cout << "First 2 For Loops:  " << size << " ints : " << diff.count() << " s\n";
+        std::cout << "Third For Loop:  " << size << " ints : " << diff_2.count() << "s\n";
+    }
 }
