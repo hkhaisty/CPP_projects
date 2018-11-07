2abf8747e6f0de1cddf0bb1fd10cce428d74d2f4
Harry Haisty <h.k.haisty@gmail.com>
2018-11-07 18:47:01 -0500

    adds timer to code

--- CSV_Helper/main.cpp
+++ CSV_Helper/main.cpp
@@ -6,10 +6,9 @@ using namespace std;
 int main() {
     ofstream myfile;
     myfile.open("example.csv");
-    myfile << "This is the first cell in the first column\n";
-    myfile << "a,b,c, \n";
-    myfile << "c,s,v, \n";
-    myfile << "1,2,3.456\n";
-    myfile << "semi;color";
+
+    for (int i = 1; i < 20; i ++){
+        myfile << rand() % 100 << "\n";
+    }
     myfile.close();
 }
--- Project/main.cpp
+++ Project/main.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 #include <algorithm>
+#include <ctime>
+#include <chrono>
 #include "methods.h"
 using namespace std;
+
+typedef chrono::high_resolution_clock Clock;
 
 int main() {
 
 
     //include standard clock
-    //read in .csv file
+    auto start_bubble_sort = Clock::now();
 
+
+    //read in .csv file
     //implement bubble_sort here
+    auto end_bubble_sort = Clock::now();
 
 
     //include standard clock end
     //calculate runtime for bubble sort
+
+    //double bubbble_sort_runtime = (double)(end_bubble_sort - start_bubble_sort);
+
+    cout << "Bubble sort runtime: " << chrono::duration_cast<chrono::nanoseconds>(end_bubble_sort - start_bubble_sort).count() << " nanoseconds." << endl;
 
     //include standard clock
     //read in .csv file again
