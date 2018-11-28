0be2f363d3c56b45772e950652d9c2f54f1b3be5
Harriet Haisty <hhaisty@students.kennesaw.edu>
2018-11-28 17:58:51 -0500

    adds code for part c

--- Big_O_Time/main.cpp
+++ Big_O_Time/main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-// problem b method
+//problem b method
 long fib(long index) {
     if (index == 0)
         return 0;
@@ -13,13 +13,14 @@ long fib(long index) {
         return fib(index - 1) + fib(index - 2);
 }
 
-// includes for loops from problem a in a method
+//includes for loops from problem a in a method
 void for_loops_a(int n_a, int sum_a) {
     for (int i = 0; i < n_a; i++)
         for (int j = 0; j < n_a * n_a; j++)
             sum_a++;
 }
 
+//includes for loops from problem d in a method
 void for_loops_d(int n_d, int sum_d) {
     for (int i = 0; i < n_d; i++)
         for (int j = 0; j < i * i; j++)
@@ -27,6 +28,7 @@ void for_loops_d(int n_d, int sum_d) {
                 sum_d++;
 }
 
+//includes for loops from problem e in a method
 void for_loops_e(int n_e, int sum_e) {
     for (int i = 1; i < n_e; i++)
         for (int j = 1; j < i * i; j++)
@@ -77,6 +79,28 @@ int main() {
     // problem b code*****************************************************************
 
     // problem c code*****************************************************************
+
+    cout << "Welcome to problem c. \n";
+
+    int main_method_n_for_problem_c;
+    int main_method_m_for_problem_c;
+
+    while (main_method_n_for_problem_c != 0){
+        cout << "Enter an integer, enter 0 to exit:";
+        cin >> main_method_m_for_problem_c;
+
+        cout << "Enter a smaller integer: ";
+        cin >> main_method_n_for_problem_c;
+
+        auto start = std::chrono::high_resolution_clock::now();
+        greatest_common_denominator(main_method_m_for_problem_c, main_method_n_for_problem_c);
+        auto end = std:: chrono::high_resolution_clock::now();
+
+        auto elapsed = (end - start);
+        auto int_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+        std::cout << "This loop took " << elapsed.count() << " milliseconds, or " << int_ms.count() << " whole milliseconds to run. \n";
+    }
 
     // problem d code*****************************************************************
 
