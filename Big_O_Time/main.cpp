73df31b823c6b37b91cafc320fe47715532d348a
Harriet Haisty <hhaisty@students.kennesaw.edu>
2018-11-28 17:17:38 -0500

    completes part d

--- Big_O_Time/main.cpp
+++ Big_O_Time/main.cpp
@@ -20,6 +20,13 @@ void for_loops_a(int n_a, int sum_a) {
             sum_a++;
 }
 
+void for_loops_d(int n_d, int sum_d) {
+    for (int i = 0; i < n_d; i++)
+        for (int j = 0; j < i * i; j++)
+            for (int k = 0; k < j; k++)
+                sum_d++;
+}
+
 // problem c method
 int greatest_common_denominator(int m, int n) {
     if (m % n == 0)
@@ -29,8 +36,10 @@ int greatest_common_denominator(int m, int n) {
 }
 
 int main() {
-    // problem a code
 
+    // problem a code*****************************************************************
+
+    cout << "Hi there, Harriet here. Here is problem a\n";
     int main_method_n_for_problem_a = 1000;
     int sum_of_a_preacher_man = 0;
 
@@ -43,7 +52,7 @@ int main() {
         //start the high resolution clock
         auto start = std::chrono::high_resolution_clock::now();
 
-        //function call to prewritted for loops for question a
+        //function call to prewritten for loops for question a
         for_loops_a(main_method_n_for_problem_a, sum_of_a_preacher_man);
 
         //end the high resolution clock
@@ -52,30 +61,37 @@ int main() {
         //subtract initial value from final clock value
         auto elapsed = (end - start);
         auto int_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-       // std::chrono::duration<double, std::milli> elapsed;
 
-        cout << "This calculation took ";
-        std::cout << elapsed.count();
-        cout << " seconds.";
+        std::cout << "This loop took " << elapsed.count() << " microseconds, "
+                  << "or " << int_ms.count() << " whole milliseconds to run.\n";
     }
 
-    cin.get();
-    cin.get();
+    // problem b code*****************************************************************
 
-    // problem b code
+    // problem c code*****************************************************************
 
-    // problem c code
-
-    // problem d code
+    // problem d code*****************************************************************
     int main_method_n_for_problem_d;
     int sum_of_cycles_problem_d = 0;
 
-    for (int i = 0; i < main_method_n_for_problem_d; i++)
-        for (int j = 0; j < i * i; j++)
-            for (int k = 0; k < j; k++)
-                sum_of_cycles_problem_d++;
+    cout << "Welcome to problem d!\n";
 
-    // problem e code
+    while (main_method_n_for_problem_d != 0) {
+
+        cout << "Enter an integer, enter 0 to exit: ";
+        cin >> main_method_n_for_problem_d;
+        auto start = std::chrono::high_resolution_clock::now();
+        for_loops_d(main_method_n_for_problem_d, sum_of_cycles_problem_d);
+        auto end = std::chrono::high_resolution_clock::now();
+
+        auto elapsed = (end - start);
+        auto int_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+        std::cout << "This loop took  " << elapsed.count() << " microseconds, or " << int_ms.count()
+                  << " whole milliseconds to run. \n";
+    }
+
+    // problem e code*****************************************************************
     int main_method_n_for_problem_e;
     int sum_of_cycles_problem_e;
 
@@ -84,6 +100,4 @@ int main() {
             if (j % i == 0)
                 for (int k = 0; k < j; k++)
                     sum_of_cycles_problem_e++;
-
-
 }
