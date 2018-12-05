31575b4cdeab228cabddfd33a4eacf2df66b06b3
hkhaisty <h.k.haisty@gmail.com>
2018-12-04 23:17:46 -0500

    adds modularization

--- part_3_10/main.cpp
+++ part_3_10/main.cpp
@@ -1,102 +1,26 @@
 #include <iostream>
-#include<math.h>
+#include <math.h>
 #include<iomanip>
+#include "methods.h"
 
 using namespace std;
 
-class complex {
-public :
-    float regular_number, complex_number;
-
-    void printNumber() {
-        if (complex_number > 0) {
-            cout << regular_number << "+" << complex_number << "i";
-        } else {
-            cout << regular_number << "" << complex_number << "i";
-        }
-
-    }
-
-    void add_numbers(complex first, complex second) {
-        regular_number = first.regular_number + second.regular_number;
-        complex_number = first.complex_number + second.complex_number;
-        if (complex_number > 0) {
-            cout << regular_number << "+" << complex_number << "i";
-        } else {
-            cout << regular_number << "" << complex_number << "i";
-        }
-
-    }
-
-    void subtract_numbers(complex one, complex two) {
-        regular_number = one.regular_number - two.regular_number;
-        complex_number = one.complex_number - two.complex_number;
-        if (complex_number > 0) {
-            cout << setprecision(2);
-            cout << regular_number << "+" << complex_number << "i";
-        } else {
-            cout << setprecision(2);
-            cout << regular_number << complex_number << "i";
-        }
-    }
-
-    void multiply_numbers(complex one, complex two) {
-        regular_number = one.regular_number * two.regular_number;
-        complex_number = one.complex_number * two.complex_number;
-        if (complex_number > 0) {
-            cout << regular_number << "+" << complex_number << "i";
-        } else {
-            cout << regular_number << "" << complex_number << "i";
-        }
-    }
-
-    void divide_numbers(complex one, complex two) {
-        if (two.regular_number == 0 || two.complex_number == 0) {
-            cout << "Sorry division by 0 is not possible";
-        } else {
-            regular_number = one.regular_number / two.regular_number;
-            complex_number = one.complex_number / two.complex_number;
-            if (complex_number > 0) {
-                cout << regular_number << "+" << complex_number << "i";
-            } else {
-                cout << regular_number << "" << complex_number << "i";
-            }
-        }
-    }
-};
-
 int main() {
-    complex first, second, result;
+    mixed_number_operations first_input_value, second_input_value, result;
 
-    cout << "Enter real part of first complex number: ";
-    cin >> first.regular_number;
+    cout << "Enter integer part of first normal number: ";
+    cin >> first_input_value.regular_number;
 
-    cout << "Enter integer of first complex number(*not* including the letter i): ";
-    cin >> first.complex_number;
+    cout << "Enter integer value of complex number(*not* including the letter i): ";
+    cin >> first_input_value.complex_number;
 
-    cout << "Enter integer part of second complex number: ";
-    cin >> second.regular_number;
+
+    cout << "Enter integer part of second normal number: ";
+    cin >> second_input_value.regular_number;
 
     cout << "Enter integer part of second complex number (*not* including the letter i): ";
-    cin >> second.complex_number;
+    cin >> second_input_value.complex_number;
 
-    cout << endl << "1. The FIRST complex number is ";
-    first.printNumber();
-
-    cout << endl << "2. The SECOND complex number is ";
-    second.printNumber();
-
-    cout << endl << "3. The SUM of the complex numbers is ";
-    result.add_numbers(first, second);
-
-    cout << endl << "4. The MULTIPLICATION of the complex numbers is ";
-    result.multiply_numbers(first, second);
-
-    cout << endl << "5. The SUBTRACTION of the complex number is ";
-    result.subtract_numbers(first, second);
-
-    cout << endl << "6. The DIVISION of the complex numbers is ";
-    result.divide_numbers(first, second);
-
-    cout << endl;
+    result.printing_results(first_input_value, second_input_value, result);
+    
 }
--- part_3_10/methods.h
+++ part_3_10/methods.h
@@ -1,17 +1,90 @@
-//
-// Created by hkhai on 12/4/2018.
-//
+using namespace std;
 
-#ifndef PART_3_10_METHODS_H
-#define PART_3_10_METHODS_H
+class mixed_number_operations {
 
-#include "main.cpp"
+public :
+    float regular_number, complex_number;
 
-class methods {
+    void printNumber() {
+        if (complex_number > 0) {
+            cout << regular_number << "+" << complex_number << "i";
+        } else {
+            cout << regular_number << "" << complex_number << "i";
+        }
+    }
 
-public:
+    void add_numbers(mixed_number_operations first, mixed_number_operations second) {
+        regular_number = first.regular_number + second.regular_number;
+        complex_number = first.complex_number + second.complex_number;
+        if (complex_number > 0) {
+            cout << regular_number << "+" << complex_number << "i";
+        } else {
+            cout << regular_number << "" << complex_number << "i";
+        }
+    }
 
+    void subtract_numbers(mixed_number_operations one, mixed_number_operations two) {
+        regular_number = one.regular_number - two.regular_number;
+        complex_number = one.complex_number - two.complex_number;
+        if (complex_number > 0) {
+            cout << setprecision(2);
+            cout << regular_number << "+" << complex_number << "i";
+        } else {
+            cout << setprecision(2);
+            cout << regular_number << complex_number << "i";
+        }
+    }
+
+    void multiply_numbers(mixed_number_operations one, mixed_number_operations two) {
+        regular_number = one.regular_number * two.regular_number;
+        complex_number = one.complex_number * two.complex_number;
+        if (complex_number > 0) {
+            cout << regular_number << "+" << complex_number << "i";
+        } else {
+            cout << regular_number << "" << complex_number << "i";
+        }
+    }
+
+    void divide_numbers(mixed_number_operations one, mixed_number_operations two) {
+        if (two.regular_number == 0 || two.complex_number == 0) {
+            cout << "Sorry division by 0 is not possible";
+        } else {
+            regular_number = one.regular_number / two.regular_number;
+            complex_number = one.complex_number / two.complex_number;
+            if (complex_number > 0) {
+                cout << regular_number << "+" << complex_number << "i";
+            } else {
+                cout << regular_number << "" << complex_number << "i";
+            }
+        }
+    }
+
+    void printing_results(mixed_number_operations first_value, mixed_number_operations second_value, mixed_number_operations result) {
+
+        cout << endl << "1. The FIRST complex number is: ";
+        first_value.
+                printNumber();
+
+        cout << endl << "2. The SECOND complex number is: ";
+        second_value.
+                printNumber();
+
+        cout << endl << "3. The SUM of the complex numbers is: ";
+        result.
+                add_numbers(first_value, second_value);
+
+        cout << endl << "4. The MULTIPLICATION of the complex numbers is: ";
+        result.
+                multiply_numbers(first_value, second_value);
+
+        cout << endl << "5. The SUBTRACTION of the complex number is: ";
+        result.
+                subtract_numbers(first_value, second_value);
+
+        cout << endl << "6. The DIVISION of the complex numbers is: ";
+        result.
+                divide_numbers(first_value, second_value);
+
+        cout << endl;
+    }
 };
-
-
-#endif //PART_3_10_METHODS_H
