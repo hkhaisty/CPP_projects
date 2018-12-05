f4606346dd5590f38bb2e26a6f8ee0d49044d7cd
hkhaisty <h.k.haisty@gmail.com>
2018-12-05 11:37:12 -0500

    adds 3.13

---
+++ part_3_13/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+//set constant integer as sentinal value 1000
+static const int N = 1000;
+
+int main()
+{
+    //initialize array
+    int i, a[N];
+
+    //loop through array starting with the 2nd element
+    for (i = 2; i < N; i++) a[i] = 1;
+
+    //loop through array
+    for (i = 2; i < N; i++)
+        if (a[i])
+            //set int j to int i value, multiply j by i until it reaches 1000, set element at i*j to 0
+            for (int j = i; j*i < N; j++) a[i*j] = 0;
+    //loop through populated array
+    for (i = 2; i < N; i++)
+        if (a[i]) cout << " " << i;
+    cout << endl;
+}
